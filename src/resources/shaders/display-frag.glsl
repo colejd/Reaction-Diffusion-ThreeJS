fea8e42bbc5165f1d1278fb9e817b22f4fef00e7
Jonathan Cole <joncole04412@gmail.com>
2021-02-05 19:36:07 -0500

    Improve display shader

--- src/resources/shaders/display-frag.glsl
+++ src/resources/shaders/display-frag.glsl
@@ -11,32 +11,44 @@ uniform sampler2D displayTexture;
 uniform vec2 resolution;
 uniform vec2 texelSize;
 
-void main() {
-    vec2 uv = v_uv;
-    vec4 pixel = texture2D( displayTexture, uv );
+vec4 classic() {
+  vec4 pixel = texture2D( displayTexture, v_uv );
 
-    //// Determine final color
-
-    // Black on white
-    float c = pixel.r - pixel.g;
-    vec4 finalColor = vec4(c, c, c, 1.0);
-
-    vec4 tint = vec4(1.0, 1.0, 1.0, 1.0);
-
-    //Apply the final color
-    gl_FragColor = finalColor * tint;
+  // Black on white
+  float c = pixel.r - pixel.g;
+  return vec4(c, c, c, 1.0);
 }
 
-void mainNew() {
-  vec2 texelSize = 1.0 / resolution.xy;
-  vec2 uv = v_uv;
-  vec4 pixel = texture2D( displayTexture, uv );
+vec4 twoTone() {
+  vec4 pixel = texture2D( displayTexture, v_uv ); // Color at the current pixel (r, g, b, a), each channel from 0 to 1
+
+  // pixel.r represents concentration of chemical A
+  // pixel.g represents concentration of chemical B
+  vec3 bgColor = vec3(1, 1, 1); // White
+  vec3 fgColor = vec3(139.0 / 255.0, 69.0 / 255.0, 19.0 / 255.0); // Saddle brown
+
+  //// Determine final color
+  float mixProgress = pixel.r - pixel.g; // Intensity of progress between fgColor and bgColor. This can be any value between 0 and 1.
+  if (mixProgress < 0.3) mixProgress = 0.0;
+  // if (mixProgress >= 0.3) mixProgress = 1.0;
+  vec3 finalColor = mix(fgColor, bgColor, mixProgress); // Linearly interpolates between the two colors. mixProgress of 0 means fgColor is used, 1 means bgColor is used.
+
+  //Apply the final color
+  return vec4(finalColor, 1.0);
+}
+
+vec4 sigmoid() {
+  vec4 pixel = texture2D( displayTexture, v_uv );
   float displayedValue = pixel.x;
 
   // Sigmoid-like function for nice edges
   const float edginess = 20.0;
   float sigmoid = 1.0 / (1.0+exp(-displayedValue * edginess + edginess * 0.5));
 
-  gl_FragColor = vec4(sigmoid);
+  return vec4(sigmoid);
 }
 
+void main() {
+  //Apply the final color
+  gl_FragColor = classic();
+}
